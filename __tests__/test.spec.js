7de5f1b28d989f6a05d81ef9ff30c8778dbf137e
Ryuta Yoneyama <ryoneyama@heroku.com>
2021-12-08 06:53:43 +0000

    Added jest and modified some files

---
+++ __tests__/test.spec.js
@@ -0,0 +1,5 @@
+describe("test of test", () => {
+    test('test of test', ()=>{
+        expect(1).toEqual(1);
+    });
+});
---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// Sync object
+/** @type {import('@jest/types').Config.InitialOptions} */
+const config = {
+    verbose: true,
+    roots: [
+        '__tests__',
+    ],
+};
+  
+module.exports = config;
+  
