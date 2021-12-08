a7fed7f39433149538dcdd41baf32b8fe9529cad
Ryuta Yoneyama <ryoneyama@heroku.com>
2021-12-08 07:21:40 +0000

    Added test

--- __tests__/test.spec.js
+++ __tests__/test.spec.js
@@ -2,4 +2,7 @@ describe("test of test", () => {
     test('test of test', ()=>{
         expect(1).toEqual(1);
     });
+    test('test of test 2', ()=>{
+        expect(2).toEqual(2);
+    });
 });
