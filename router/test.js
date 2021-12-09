b86914a70dd2801fb54bfad1c182399ca95dbf84
Ryuta Yoneyama <ryoneyama@heroku.com>
2021-12-09 06:42:49 +0000

    Modified router

--- router/test.js
+++ router/test.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models/index');
 
-router.get('/',async (req,res)=>{
+router.get('/test1',async (req,res)=>{
     const tests = await db.Test.findAll({});
     res.send(tests);
 });
