a53b08e2c09874e4c2df87d83f5eff3d96476a21
Ryuta Yoneyama <ryoneyama@heroku.com>
2021-12-09 06:30:57 +0000

    Modified a bit

--- __tests__/db.spec.js
+++ __tests__/db.spec.js
@@ -1,7 +1,7 @@
 const db = require('../models/index');
 process.env['NODE_ENV'] = process.env['NODE_ENV'] || 'development'; // for local-dev
 describe("db test", () => {
-    test('test table >= 0',async ()=>{
+    test('"Test" exists',async ()=>{
         const test = await db.Test.findAll({});
         expect(test.length).toBeGreaterThanOrEqual(0);
     });
